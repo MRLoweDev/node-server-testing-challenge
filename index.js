b6bdbc1beecc470c187180ddc5f853b3f7f7c375
Michael Lowe <mrlowememphis@yahoo.com>
2020-05-15 10:58:27 -0500

    MVP MET Test working!

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require('./api/server.js');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => console.log(` Running on port: ${PORT} `)); 
+server.listen(PORT, () => console.log(` Running on port: ${PORT} `));
